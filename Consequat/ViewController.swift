dbf019e373c71e66288f3156a530ece433bb0e6d
Quentin CLEMENT <quentinclement@MacBook-Pro-de-Quentin.local>
2018-10-27 16:26:30 +0200

    Master Project creation

---
+++ Consequat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Consequat
+//
+//  Created by Quentin CLEMENT on 27/10/2018.
+//  Copyright © 2018 Quentin CLEMENT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
