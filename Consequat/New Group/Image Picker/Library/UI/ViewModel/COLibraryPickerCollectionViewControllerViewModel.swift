7e45c91dee1fbbb8846710cb3962c3bfeb5cc2ed
Quentin c <quentinclement@macbook-pro-de-quentin.local>
2018-10-28 09:40:34 +0100

    feature/1.0/AktMVVO7 Prepare Library Picker View controller

---
+++ Consequat/New Group/Image Picker/Library/UI/ViewModel/COLibraryPickerCollectionViewControllerViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  COLibraryPickerCollectionViewControllerViewModel.swift
+//  Consequat
+//
+//  Created by Quentin CLEMENT on 28/10/2018.
+//  Copyright © 2018 Quentin CLEMENT. All rights reserved.
+//
+
+import QCViewModel
+
+class COLibraryPickerCollectionViewControllerViewModel: QCCollectionViewModel {
+
+}
