87a5c89e1c4dbd0067109dac9746e1cf963b7dab
Quentin c <quentinclement@macbook-pro-de-quentin.local>
2018-10-28 19:40:27 +0100

    feature/1.0/sxIf7RKP Add Customizable Image View

---
+++ Consequat/New Group/Image Customization/UI/ViewController/COImageCustomizationViewController.swift
@@ -0,0 +1,47 @@
+//
+//  COImageCustomizationViewController.swift
+//  Consequat
+//
+//  Created by Quentin CLEMENT on 28/10/2018.
+//  Copyright © 2018 Quentin CLEMENT. All rights reserved.
+//
+
+import UIKit
+
+class COImageCustomizationViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var photoImageView: UIImageView?
+
+    let viewModel: COImageCustomizationViewControllerViewModel?
+    
+    // MARK: - Initialization
+    
+    init(viewModel: COImageCustomizationViewControllerViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: String(describing: COImageCustomizationViewController.self),
+                   bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.viewModel = nil
+        super.init(nibName: String(describing: COImageCustomizationViewController.self),
+                   bundle: nil)
+    }
+    
+    // MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.photoImageView?.image = self.viewModel?.image
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func moreButtonTouchUpInside(_ sender: UIButton) {
+        self.viewModel?.addEmoji(on: self.photoImageView)
+    }
+
+}
